c5493fc359e55fe7b2ab40e6d81357b702192bc7
BradleyWilliamsDev <Bradley.Williams0511@gmail.com>
2023-03-14 09:43:49 +0200

    GPT3 Website Design Progress

---
+++ src/components/brand/imports.js
@@ -0,0 +1,16 @@
+// Helper file to import everything
+// Allowing us to import into the main file in one line
+
+import google from '../../Assets/google.png';
+import slack from '../../Assets/slack.png';
+import atlassian from '../../Assets/atlassian.png';
+import dropbox from '../../Assets/dropbox.png';
+import shopify from '../../Assets/shopify.png';
+
+export{
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './possibility.css';
+
+import possibilityImage from '../../Assets/possibility.png';
+
+const Possibility = () => {
+  return (
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src={possibilityImage} alt='possibility' />
+      </div>
+    </div>
+  )
+}
+
+export default Possibility
